f20335bc52d50f517ccf134ad3cc59d64d46543b
Cqban <54151734+Cqban@users.noreply.github.com>
2023-05-22 08:18:30 +0200

    Update settings.py
    Improved cross-machines compatibility

--- computermngt/settings.py
+++ computermngt/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'computermngt.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
